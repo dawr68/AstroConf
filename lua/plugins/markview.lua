e1a7563c6d9c65d92a9cbfe86ef40b5b9467fd50
Dawid Rudy <dawid@auterion.com>
2025-09-02 12:08:58 +0200

    feat: add markdown viewer plugin

---
+++ lua/plugins/markview.lua
@@ -0,0 +1,16 @@
+
+---@type LazySpec
+-- For `plugins/markview.lua` users.
+return {
+    "OXY2DEV/markview.nvim",
+    lazy = false,
+
+   -- For `nvim-treesitter` users.
+    priority = 49,
+
+    -- For blink.cmp's completion
+    -- source
+    -- dependencies = {
+    --     "saghen/blink.cmp"
+    -- },
+};
